6711be80d66a39ca5d53d396d2c3b719ea732456
Vasyl Koperdas <koperdasb@gmail.com>
2022-09-04 13:41:06 +0300

    create favorite screen

--- lib/src/ui/favorite/favorite_screen.dart
+++ lib/src/ui/favorite/favorite_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:kopa/src/ui/home/components/list_card_widget.dart';
 
 class FavoriteScreen extends StatelessWidget {
   const FavoriteScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Favorite'));
+    return Column(
+      children: const [
+        ListCardWidget(),
+      ],
+    );
   }
 }
